67cee23fa5e508d429aa79c0eeb0e6316c99f8d7
Eduardo A. Paris Penas <edward2a@gmail.com>
2024-10-03 16:13:44 +0100

    add basic validating webhook which approves all requests

---
+++ source/webhook.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import falcon
+
+from wsgiref.simple_server import make_server
+
+class HealthHandler(object):
+
+    def on_get(self, req, resp):
+        """Return HTTP/204."""
+        resp.status = falcon.HTTP_NO_CONTENT
+
+class ValidatingWebhookHandler(object):
+
+    def on_post(self, req, resp):
+        """Process validation request and respond."""
+        print(req.media)
+        resp.media = {
+            "response": {
+                "allowed": True,
+                "uid": req.media["uid"],
+                "status": {"message": "default approval"}
+            }
+        }
+
+
+def main():
+    app = falcon.App()
+    app.add_route('/health', HealthHandler())
+    app.add_route('/validate', ValidatingWebhookHandler())
+
+    with make_server('0.0.0.0', 8080, app) as httpd:
+        httpd.serve_forever()
+
+if __name__ == '__main__':
+    main()

0a04a16513acb70cfb8b4935bec9ac07c709b8c3
Eduardo A. Paris Penas <edward2a@gmail.com>
2025-05-09 09:51:20 +0100

    Add basic tls server support

--- source/webhook.py
+++ source/webhook.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import falcon
+import ssl
 
 from wsgiref.simple_server import make_server
 
@@ -25,6 +26,8 @@ class ValidatingWebhookHandler(object):
             }
         }
 
+    def on_get(self, req, resp):
+        resp.media = {'msg': 'Hello, world!'}
 
 
 def main():
@@ -33,6 +36,13 @@ def main():
     app.add_route('/validate', ValidatingWebhookHandler())
 
     with make_server('127.0.0.1', 8081, app) as httpd:
+        ctx = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+        ctx.load_cert_chain('source/cert.pem','source/key.pem')
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+        httpd.socket = ctx.wrap_socket(
+            httpd.socket,
+            server_side=True)
         httpd.serve_forever()
 
 if __name__ == '__main__':
